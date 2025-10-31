c385c4b546e1ea4c24be82f13ba987c1416e251c
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-01 00:09:13 +1000

    Complete word_occurrences milestone

---
+++ prac_05/world_occurrences.py
@@ -0,0 +1,8 @@
+text = input("Text: ")
+words = text.split()
+word_to_count = {}
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+max_length = max(len(word) for word in words)
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")
