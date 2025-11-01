1ab6292570042421c86e894f093fcf544235dca4
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 08:54:55 +1000

    Print names of dynamically typed languages using is_dynamic method

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -12,3 +12,9 @@ def main():
     print(python)
 
     languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+main()
