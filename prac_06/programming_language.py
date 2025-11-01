16dfec03b2206e71a88b8cfc19496c772aef4372
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 08:31:14 +1000

    Add ProgrammingLanguage class with init function

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,3 +2,13 @@
 Estimate: 45 minutes
 Actual: 30 minutes
 """
+
+class ProgrammingLanguage:
+    """Represent a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Create a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
