4fa86b44fdde9d775a16a21a93f45f067cf4bd1d
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 08:47:57 +1000

    Add main function using these lines

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,3 +4,9 @@ Actual: 20 minutes
 """
 
 from prac_06.programming_language import ProgrammingLanguages
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
