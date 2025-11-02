44d5098ee27dc33d3d145dd011c422f7b5cbc232
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 15:01:14 +1000

    Add the data for testing

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,3 +14,6 @@ def main():
         guitars.append(guitar_to_add)
         print(guitar_to_add, "added.")
         name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
