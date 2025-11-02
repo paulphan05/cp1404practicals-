b30fa220658b2d2f6a7ad4ee5749a47577b4f3a5
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 14:59:59 +1000

    Add main program and display guitars using Guitar class

---
+++ prac_06/guitars.py
@@ -0,0 +1,16 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Guitar program, using Guitar class."""
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
