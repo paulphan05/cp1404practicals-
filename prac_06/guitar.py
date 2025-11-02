dc50749104f58086f3e5504474d79b5c3dfd3fc2
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 14:42:22 +1000

    Add the __str__ method to display guitar details

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -11,3 +11,7 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a formatted string representation of the guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
