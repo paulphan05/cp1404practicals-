d98fc9c905df1d945a0580e9cf3cbc940e9bfda8
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 14:50:11 +1000

    Add run_tests function with the example from prac 2

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,3 +5,12 @@ Actual time: 32 minutes
 from prac_06.guitar import Guitar
 
 CURRENT_YEAR = 2025
+
+def run_tests():
+    """Tests for Guitar class."""
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    other = Guitar("Another Guitar", 2016, 1512.9)
