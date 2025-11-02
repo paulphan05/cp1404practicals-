41201a0bdb9cdfed1b5c35e4147ea71f5f4d80f5
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 15:05:15 +1000

    Add for loop and using enumerate function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,3 +17,8 @@ def main():
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
