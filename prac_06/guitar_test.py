8fe18d2388ed1843075e1da0c032102d420db69f
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 14:51:59 +1000

    Add the print function for get_age and is_vintage method

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -14,3 +14,13 @@ def run_tests():
 
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2016, 1512.9)
+
+    print(f"{guitar.name} get_age() - Expected {103}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {9}. Got {other.get_age()}")
+    print()
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+
+
+if __name__ == '__main__':
+    run_tests()
