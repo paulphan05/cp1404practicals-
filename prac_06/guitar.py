0d79b763eb37dd2d0a9e3a379409e2ee7e198470
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-02 14:39:59 +1000

    Add the constant with guitar class and init function

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,3 +2,12 @@
 Estimate: 20 minutes
 Actual: 10 minutes
 """
+current_year = 2025
+class Guitar:
+    """Represent a guitar with name, year, and cost."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
