56012174983688e99c38f0abbe5b9c79162d8846
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-06 18:41:07 +1000

    Complete first part of More Guitars exercise

---
+++ prac_07/guitar.py
@@ -0,0 +1,31 @@
+"""
+Estimate: 20 minutes
+Actual: 10 minutes
+"""
+current_year = 2025
+
+
+class Guitar:
+    """Represent a guitar with name, year, and cost."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a formatted string representation of the guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years."""
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, False otherwise."""
+        return self.get_age() >= 50
+
+    def __lt__(self, other):
+        """Compare Guitars by year for sorting."""
+        return self.year < other.year
