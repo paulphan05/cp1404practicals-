9bc5732bc9aaac9bd53e80e222105d24643734f1
paulphan05 <giabao.phan@my.jcu.edu.au>
2025-11-06 18:38:49 +1000

    Complete first part of More Guitars exercise

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,7 +2,7 @@
 Estimate: 20 minutes
 Actual: 10 minutes
 """
-current_year = 2025
+CURRENT_YEAR = 2025
 
 
 class Guitar:
@@ -20,8 +20,12 @@ class Guitar:
 
     def get_age(self):
         """Return the age of the guitar in years."""
-        return current_year - self.year
+        return CURRENT_YEAR - self.year
 
     def is_vintage(self):
         """Return True if the guitar is 50 or more years old, False otherwise."""
         return self.get_age() >= 50
+
+    def __lt__(self, other):
+        """Compare Guitars by year for sorting."""
+        return self.year < other.year
